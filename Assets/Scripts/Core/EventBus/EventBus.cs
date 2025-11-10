df75014e943c01be8ad1fdbcacad594f463c5988
Serhiy <216123941+Crafter2k1@users.noreply.github.com>
2025-11-10 13:58:37 +0200

    Init

---
+++ Assets/Scripts/Core/EventBus/EventBus.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace Core.EventBus
+{
+    public static class EventBus
+    {
+        private static readonly Dictionary<Type, List<Delegate>> _subscribers = new();
+
+        public static void Subscribe<T>(Action<T> callback)
+        {
+            var type = typeof(T);
+            if (!_subscribers.ContainsKey(type))
+                _subscribers[type] = new List<Delegate>();
+
+            _subscribers[type].Add(callback);
+        }
+
+        public static void Unsubscribe<T>(Action<T> callback)
+        {
+            var type = typeof(T);
+            if (_subscribers.TryGetValue(type, out var list))
+            {
+                list.Remove(callback);
+                if (list.Count == 0)
+                    _subscribers.Remove(type);
+            }
+        }
+
+        public static void Invoke<T>(T eventData)
+        {
+            var type = typeof(T);
+            if (_subscribers.TryGetValue(type, out var list))
+            {
+                foreach (var callback in list)
+                    (callback as Action<T>)?.Invoke(eventData);
+            }
+        }
+    }
+}
